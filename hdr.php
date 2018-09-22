5f9bba54459dc69c4a0346f780b506ddeac86fbb
triagustinaputri <42725273+triagustinaputri@users.noreply.github.com>
2018-09-22 10:58:37 +0700

    Add files via upload

---
+++ hdr.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>latihan</title>
+</head>
+<body bgcolor="grey">
+	<div style="background-color: black;">
+		<marquee>
+			<img src="kc.jpg" width="50">
+			<font size="10" color="blue">SELAMAT DATANG </font>
+			<img src="kc.jpg" width="50">
+		</marquee>
+		<hr>
+	</div>
+</body>
+</html>
